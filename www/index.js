44407904390a11237224c36b38222dbdfac28c56
rjgc <rj.leiria@gmail.com>
2020-11-02 11:45:12 +0000

    Update URL

--- www/index.js
+++ www/index.js
@@ -34,7 +34,7 @@ var app = {
     
     onBackButton: function(event) {
         event.preventDefault();
-        if (window.location.href == 'https://mediaweb.pt/projects/code-reader/index.html') {
+        if (window.location.href == 'http://62.48.151.76:8000/NOX/POS/APPS/Login/') {
 
         } else {
             navigator.app.backHistory();
